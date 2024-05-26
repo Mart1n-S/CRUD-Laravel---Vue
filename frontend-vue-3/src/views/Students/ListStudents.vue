92686e4bdcfcc638e5cf335815520bac9832bfbc
Martin <martinsimongo@gmail.com>
2024-05-26 17:15:33 +0200

    suppression d'un étudiant + ajout d'un spinner lors du chargement de la liste des étudiant + readme

--- frontend-vue-3/src/views/Students/ListStudents.vue
+++ frontend-vue-3/src/views/Students/ListStudents.vue
@@ -29,13 +29,18 @@
                         <td>
                             <RouterLink :to="{ path: '/students/' + student.id + '/edit' }"
                                 class="btn btn-success float-end w-100">Edit</RouterLink>
-                            <button type="button" class="btn btn-danger float-end w-100 my-2">Supprimer</button>
+                            <button type="button" @click="deleteStudent(student.id)"
+                                class="btn btn-danger float-end w-100 my-2">Supprimer</button>
                         </td>
                     </tr>
                 </tbody>
                 <tbody v-else>
                     <tr>
-                        <td colspan="7">Loading</td>
+                        <td colspan="7" class="text-center">
+                            <div class="d-flex justify-content-center align-items-center">
+                                <orbit-spinner :animation-duration="1200" :size="55" color="#41B883" />
+                            </div>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -46,8 +51,12 @@
 
 <script>
 import axios from 'axios';
+import { OrbitSpinner } from 'epic-spinners'
 
 export default {
+    components: {
+        OrbitSpinner,
+    },
     name: 'students',
     data() {
         return {
@@ -55,13 +64,32 @@ export default {
         }
     },
     mounted() {
-        this.getStudents();
+        setTimeout(() => {
+            this.getStudents();
+        }, 1500);
+
     },
     methods: {
         getStudents() {
             axios.get('http://127.0.0.1:8000/api/students').then(resultat => {
                 this.students = resultat.data.students
             });
+        },
+        deleteStudent(studentId) {
+            console.log(studentId)
+            if (confirm('Êtes-vous sur de vouloirs supprimés cet étudiant ? Cette action est irréversible.')) {
+                axios.delete(`http://127.0.0.1:8000/api/students/${studentId}/delete`).then(resultat => {
+                    alert(resultat.data.message);
+                    this.getStudents();
+                })
+                    .catch(function (error) {
+                        if (error.response) {
+                            if (error.response.status === 404) {
+                                alert(error.response.data.message);
+                            }
+                        }
+                    });
+            }
         }
     }
 }
